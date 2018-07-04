b50e0ad992f072ca2885e9593d6279266ccd72d4
111333 <111333222>
2018-07-04 15:10:59 +0800

    eureka 增加事业监听器；provide-service中eureka整合swagger；
    eureka 增加事业监听器；provide-service中eureka整合swagger；

--- eureka/provide-service/src/main/java/com/zfh/provide/Application.java
+++ eureka/provide-service/src/main/java/com/zfh/provide/Application.java
@@ -7,8 +7,10 @@ package com.zfh.provide;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-@SpringBootApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@SpringBootApplication
+@EnableSwagger2
 @EnableEurekaClient
 public class Application {
 /*************  访问地址：http://localhost:8888/user/dev  ****************/

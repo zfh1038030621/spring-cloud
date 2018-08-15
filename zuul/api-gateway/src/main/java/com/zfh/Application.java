e0545150ed81500507a8c820e4d3318a9525a5cc
111333 <111333222>
2018-08-15 17:12:54 +0800

    完成在网关中集合所有项目的swagger api 数据
    完成在网关中集合所有项目的swagger api 数据

--- zuul/api-gateway/src/main/java/com/zfh/Application.java
+++ zuul/api-gateway/src/main/java/com/zfh/Application.java
@@ -10,6 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 /*支持网关路由*/
@@ -26,5 +33,26 @@ public class Application {
     public TestFilter testFilter(){
         return  new TestFilter();
     }
+
+    @Component
+    @Primary
+    class DocumentationConfig implements SwaggerResourcesProvider {
+        @Override
+        public List<SwaggerResource> get() {
+            List resources = new ArrayList<>();
+            resources.add(swaggerResource("service-a", "/swagger-service-a/v2/api-docs", "2.0"));
+            resources.add(swaggerResource("service-b", "/swagger-service-b/v2/api-docs", "2.0"));
+            return resources;
+        }
+
+        private SwaggerResource swaggerResource(String name, String location, String version) {
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setName(name);
+            swaggerResource.setLocation(location);
+            swaggerResource.setSwaggerVersion(version);
+            return swaggerResource;
+        }
+    }
+
 }
 

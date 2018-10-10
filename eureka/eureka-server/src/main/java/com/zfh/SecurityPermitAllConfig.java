db10cef08e60127f1cd69183a08bee1758b29f86
111333 <111333222>
2018-10-10 11:49:13 +0800

    eureka server增加admin2.0 client
    eureka server增加admin2.0 client;
    springboot，spring could版本都换成最新版本

---
+++ eureka/eureka-server/src/main/java/com/zfh/SecurityPermitAllConfig.java
@@ -0,0 +1,17 @@
+package com.zfh;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 开放所有请求，能让admin2.0能访问一些监控端口
+ */
+@Configuration
+public  class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+    }
+}

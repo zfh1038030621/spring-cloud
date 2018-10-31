50e1610e280c2467136116d3e51d6975186bcf97
111333 <111333222>
2018-10-31 14:51:22 +0800

    eureka

--- eureka/eureka-server/src/main/java/com/zfh/SecurityPermitAllConfig.java
+++ eureka/eureka-server/src/main/java/com/zfh/SecurityPermitAllConfig.java
@@ -2,16 +2,26 @@ package com.zfh;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * 开放所有请求，能让admin2.0能访问一些监控端口
  */
+
+
 @Configuration
+@EnableWebSecurity
 public  class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+       // http.authorizeRequests().anyRequest().authenticated()
+       /* http.authorizeRequests().antMatchers("/actuator/**").permitAll().anyRequest().authenticated()
+                .and().formLogin()
+                .and().csrf().disable();*/
+
         http.authorizeRequests().anyRequest().permitAll()
                 .and().csrf().disable();
     }
 }
+
